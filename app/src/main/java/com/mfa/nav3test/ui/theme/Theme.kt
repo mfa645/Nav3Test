fa4403da6e1d83bd304cf1fa89ca7815096e11aa
Mariano Fernandez <keybito11@gmail.com>
2025-06-18 19:02:37 +0200

    Removed unused imports

--- app/src/main/java/com/mfa/nav3test/ui/theme/Theme.kt
+++ app/src/main/java/com/mfa/nav3test/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mfa.nav3test.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
